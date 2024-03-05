66b5c555a39330c74f58576932f96a8f7469ff7e
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-03-05 11:39:11 -0600

    working on desktop

--- kitchenaid/components/Categories/index.js
+++ kitchenaid/components/Categories/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Category from "./components/Category";
-
 import { v4 as uuidv4 } from "uuid";
 
 export const Categories = ({
