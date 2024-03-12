2a3d72cb32e5105dc3ed6e7e44c1f152845d59bb
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-03-12 11:05:02 -0600

    new project

--- Grohe/components/Header/index.js
+++ Grohe/components/Header/index.js
@@ -1,37 +1,7 @@
-import PropTypes from "prop-types";
+import React from "react";
 
-import Image from "next/image";
-import Arrow from "./components/Arrow";
-
-const Header = ({ imgCover = "", title, paragraph }) => {
-  return (
-    <header className="Header">
-      <div className={imgCover}>
-        <div className="Header-container ">
-          <div className="Header-content">
-            <figure className="Header-logo">
-              <Image
-                className="Header-image"
-                height={52}
-                width={104}
-                alt="Header-Image"
-                priority
-                src="/BRAND-LOGO.png"
-              />
-            </figure>
-            <h1 className="Header-title">{title}</h1>
-            <p className="Header-paragraph">{paragraph}</p>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-};
-
-Header.propTypes = {
-  imgCover: PropTypes.string,
-  title: PropTypes.string.isRequired,
-  paragraph: PropTypes.string.isRequired,
+const Header = () => {
+  return <div className="Header"></div>;
 };
 
 export default Header;
--- Grohe/pages/index.js
+++ Grohe/pages/index.js
@@ -1,11 +1,4 @@
-import Carousel from "@/components/Carousel";
-import Categories from "@/components/Categories";
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Offer from "@/components/Offer";
-
-import Sticky from "@/components/Sticky";
-import { categories, secondcategories } from "@/constants/dataList";
 
 import Head from "next/head";
 
@@ -19,14 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Header
-          logoHeight={52}
-          logoWidth={104}
-          title="Grohe"
-          imgCover="Header-cover"
-          imgArrow="Arrow-img"
-          paragraph="Entdecke dein exklusives Angebot"
-        />
+        <Header />
       </main>
     </>
   );
