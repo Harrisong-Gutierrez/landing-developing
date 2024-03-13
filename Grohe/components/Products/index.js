5d0cb7a932ef4827c1cd1aef732c537512860635
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-03-13 16:31:12 -0600

    solving QA

--- Grohe/components/Products/index.js
+++ Grohe/components/Products/index.js
@@ -13,24 +13,26 @@ const Products = () => {
         Riduce il calcare, il residuo fisso, e migliora eccezionalmente il gusto
         dell'acqua.
       </p>
-      <Product
-        titleClassVariant="Product-title-secondVariant"
-        titleClass="Product-title Product-title-firstVariant Product-title-firstVariantOne"
-        btnText="Scopri GROHE Blue Pure"
-        logoImg="/VEEPEE.png"
-        imageSrc="/product1-mobile.png"
-        title="GROHE Blue Pure"
-        titleVariant="Rubinetto con filtro"
-        subtitleClass="Product-subtitle"
-        subtitle="Acqua filtrata naturale a temperatura ambiente dal gusto delizioso."
-      />
-      {/* <Product
-        btnText="Scopri GROHE Blue Home"
-        logoImg="/VEEPEE.png"
-        imageSrc="/product2-mobile.png"
-        title="GROHE Blue Home Rubinetto con filtro e frigo gasatore"
-        subtitle="Acqua naturale fresca, leggermente frizzante e frizzante fredda, tutto da un unico rubinetto."
-      /> */}
+      <div className="Products-cards">
+        <Product
+          titleClassVariant="Product-title-secondVariant"
+          titleClass="Product-title Product-title-firstVariant Product-title-firstVariantOne"
+          btnText="Scopri GROHE Blue Pure"
+          logoImg="/VEEPEE.png"
+          imageSrc="/product1-mobile.png"
+          title="GROHE Blue Pure"
+          titleVariant="Rubinetto con filtro"
+          subtitleClass="Product-subtitle"
+          subtitle="Acqua filtrata naturale a temperatura ambiente dal gusto delizioso."
+        />
+        {/* <Product
+          btnText="Scopri GROHE Blue Home"
+          logoImg="/VEEPEE.png"
+          imageSrc="/product2-mobile.png"
+          title="GROHE Blue Home Rubinetto con filtro e frigo gasatore"
+          subtitle="Acqua naturale fresca, leggermente frizzante e frizzante fredda, tutto da un unico rubinetto."
+        /> */}
+      </div>
     </section>
   );
 };
