7c2d47b41735e78695e95ff5a401b7288a1dc68e
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-03-15 11:52:58 -0600

    Groke

--- Grohe/components/Selections/index.js
+++ Grohe/components/Selections/index.js
@@ -3,14 +3,16 @@ import React from "react";
 const Selections = () => {
   return (
     <section className="Selections">
-      <h2 className="Selections-title">GROHE Blue È UNA SCELTA SOSTENIBILE</h2>
+      <h2 className="Selections-title">GROHE Blue è una scelta sostenibile</h2>
       <p className="Selections-firstText">
         Salviamo il pianeta un bicchiere alla volta e diciamo addio alle
         bottiglie di plastica!
       </p>
       <p className="Selections-secondText">
-        Solo su Veepee per un tempo limitato con sconto fino
-        <span>a 50% e altri imperdibili vantaggi!</span>
+        Solo su Veepee per un tempo limitato con sconto fino ,
+        <span className="Selections-secondText-variant">
+          a 50% e altri imperdibili vantaggi!
+        </span>
       </p>
     </section>
   );
