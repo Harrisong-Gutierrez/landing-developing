7b41c26aff57d6fcb28c9425b676caff5b3b0e9a
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-03-22 14:49:20 -0600

    home

--- Grohe/components/Header/components/HeaderSecondary/index.js
+++ Grohe/components/Header/components/HeaderSecondary/index.js
@@ -1,18 +1,34 @@
 import Button from "@/components/Button";
+import Image from "next/image";
 import React from "react";
 import { IoIosArrowDown } from "react-icons/io";
 
 const HeaderSecondary = ({ onReturnButtonClick }) => {
   return (
-    <div>
-      <Button
-        styleLink="Header-secondary-button"
-        textContent="Volver al header principal"
-        onClick={onReturnButtonClick}
-        href="/"
-      />
-      <IoIosArrowDown className="Header-button-icon" />
-    </div>
+    <section className="HeaderSecondary">
+      <figure className="HeaderSecondary-logo">
+        <Image
+          className="Header-logo"
+          src="/header-logo.png"
+          alt="header-logo"
+          width={78}
+          height={52}
+        />
+      </figure>
+      <div className="HeaderSecondary-content">
+        <h2 className="HeaderSecondary-title">
+          SISTEMI DI FILTRAZIONE GROHE BLUE
+        </h2>
+        <Button
+          styleLink="HeaderSecondary-button"
+          className="Header-button-text"
+          textContent="Scopri l'offerta"
+          onClick={onReturnButtonClick}
+          href="/"
+        />
+        <IoIosArrowDown className="Header-button-icon" />
+      </div>
+    </section>
   );
 };
 
